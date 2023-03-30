5d59070154bee398991fee626985c82f54df22da
agustinlezcano <lezcanoagustin2012@gmail.com>
2023-03-30 12:19:30 -0300

    agregado modelo de carro y valores

--- Simulink/Valores.m
+++ Simulink/Valores.m
@@ -25,6 +25,24 @@ Thm_Max=2e4;%[N*m] torque maximo de motorizacion
 %% Parametros del cable de Izaje (wirerope)
 kwu=2.36e8;%(N/m)*m Rigidez unitaria de a traccion
 bwu=150;%[N/m/s]/m Friccion interna o vizcosa unitaria a traccion 
-
-
-
+%% Carro y cable de acero (wirerope) equivalente
+Mt=30000; %[kg] Masa equivalente de Carro, ruedas, etc
+bt=90.0; %[N/(m/s)] Coeficiente de Fricción mecánica viscosa equivalente del Carro
+Ktw=4.8e5; %[N/m]   Rigidez equivalente total a tracción de cable tensado de carro
+btw=3.0e3; %[N/(m/s)] Fricción interna o amortiguamiento total de cable tensado de carro
+%% Parametros del carro (traslacion)
+rtd=0.5; %[m]
+Jtd=1200; %[kg.m^2] 
+btd=1.8; %[N*m/rad/s] coef de friccion mecanica viscosa equiv. eje lento
+it=30; %relacion de transmision total caja reductora
+Jtm_tb=7.0; %[kg.m^2] Momento de inercia equivalente del eje rápido
+btm=6.0; %[N*m/rad/s] coef de friccion mecanica viscosa equiv. eje rapido
+btb=5e6; %[N*m/rad/s] Coeficiente de Fricción viscosa equivalente del Freno de operación
+Ttb_Max=5e3; %[N.m] Torque máximo de frenado del Freno de operación
+tau_tm=1e-3; %[s] Constante de tiempo de Modulador de Torque en motor-drive de carro
+Ttm_Max=3e3; %[N.m] Torque máximo de motorización/frenado regenerativo del motor
+%Parámetros equivalentes
+Meq=(rtd*Mt*it-Jtd)/rtd;
+beq=(btd+rtd*bt*it)/rtd;
+Meqc=Meq+it;    %Es Meq+it*M2 ver que es M2->corregir
+beqc=it*btm/rtd+beq; %corregir
