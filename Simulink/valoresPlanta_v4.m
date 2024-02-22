af5619731222d72ca0aff54b3100af0cd776189d
agustinlezcano <lezcanoagustin2012@gmail.com>
2024-02-22 19:32:18 -0300

    valores de freno y trayectoria

--- Simulink/valoresPlanta_v4.m
+++ Simulink/valoresPlanta_v4.m
@@ -1,9 +1,15 @@
 clc;
 clear;
 close all;
+%% Parameros de dibujo
+xmin = -35;
+xmax = 50;
+ymin = -25;
+ymax = 55;
+%% Masa, coeficienes de friccion
 bcx=1e6;%[N/m/s]
 bcy=1e7;%[N/m/s]
-kcy=1.8e9;%[N/m]
+kcy=1.8e9;%[N/m] 
 Yt0=45;%[m]
 Hc=2.50;%[m] ¿2.5 m?
 Ms=1.5e4;%[kg]
@@ -104,7 +110,7 @@ P = 956.15e3; %Potencia cte: 956.15 kW
 %whmMax = (3*2/rhd)*ih; %rad/s
 whmMax = P/Thm_Max;
 wtmMax = (4/rtd)*it; %v_m: 4 [m/s]
-v_x_max = 4; %v_m: 4 [m/s] -> por ahora lo uso para aceleracion maxima
+vx_max = 4; %v_m: 4 [m/s] -> por ahora lo uso para aceleracion maxima
 ml=5e4;
 ax_max = 0.8; %[m/s^2]
 ay_max = 0.75; %[m/s^2]
@@ -117,20 +123,36 @@ atmax=0.8;
 alfatmax=atmax/rtd;
 
 %% Posiciones de referencia para consigna
-% Perfil de contenedores
-x_positions = 2.5:2.5:50;
+% [-28.7500000000000,-26.2500000000000,-23.7500000000000,-21.2500000000000,-18.7500000000000,-16.2500000000000,-13.7500000000000,-11.2500000000000,-8.75000000000000,-6.25000000000000,-3.75000000000000,-1.25000000000000,1.25000000000000,3.75000000000000,6.25000000000000,8.75000000000000,11.2500000000000,13.7500000000000,16.2500000000000,18.7500000000000,21.2500000000000,23.7500000000000,26.2500000000000,28.7500000000000,31.2500000000000,33.7500000000000,36.2500000000000,38.7500000000000,41.2500000000000,43.7500000000000,46.2500000000000,48.7500000000000;
+%   0,0,0,0,0,0,0,0,0,0,0,0,17,21,-15,22,9,-16,-8,5,24,24,-13,24,24,2,16,-14,-1,22,16,24]
+
+% Posiciones deseadas: [Inicial(real), barco, barco_2, final(muelle)]
+p_referencia = [-20, 2.5; 30.5, 30; 20.5, 25; -20, 2.5];
+
+
+% Perfil de contenedores (arrancar desde -30)
+% x_positions = 1.25:2.5:(50-1.25);
+x_positions = (-30+1.25):2.5:(50-1.25);
+% Perfil en muelle
+%hy_muelle = randi([0, 2], 1, 12);
+hy_muelle = zeros(1,12);
 % Altura de los contenedores
 hy_cont = randi([0, 45], 1, 20);
-
 
 % Ancho de cada contenedor
 hx_cont = 2.5;
 boat_under_water = 20;
 hy_cont = hy_cont - boat_under_water; % cambio referencia
-estado_cont = [x_positions; hy_cont]
+estado_cont = [x_positions; [hy_muelle hy_cont]]
+% estado_cont = [x_positions; hy_cont]
 
-%posicion de referencia
-pos = puntosTrayectoria(4, 1.5, 20, [-20,0], estado_cont, hy_cont, 7)
+%posicion de referencia (si uso 0 es el piso) --> error en balanceo
+% la posicion final debe ser mayor a 12 (0 .. 12 muelle)
+% posicion inicial:[-20,5]
+% pos = puntosTrayectoria(4, 1.5, 20, [-20,0], estado_cont, hy_cont, 7)
+
+% calcularTrayectoria2(VX_MAX, VY_MAX, AX_MAX, dy, anchoCont, pos_f, pos_i, to_where, estado_cont)
+pos = calcularTrayectoria2_prueba(vx_max, vhmax, ax_max, Hc, hx_cont, [30.5,30], [-20,2.5], 0, estado_cont)
 plot(pos(:, 1), pos(:, 2), 'o-', 'LineWidth', 2);
 
 % Etiqueta los ejes
@@ -138,6 +160,22 @@ xlabel('Posición en x');
 ylabel('Altura');
 title('Trayectoria de puntos');
 hold on
-plot(estado_cont(1,:), estado_cont(2,:), 'o-', 'LineWidth', 2);
+% plot(estado_cont(1,:), estado_cont(2,:), 'o-', 'LineWidth', 2);
+b = bar(estado_cont(1,13:32), estado_cont(2,13:32), 1);
+b(1).BaseValue = -20;
 hold on
+%viga voladiza
+plot([-30 50],[45 45 ],'Color','k')
+hold on
+% %cable
+% cable = plot([xt xl],[45 yl+Hc],'Color','k');
+% hold on
+%barco
+rectangle('Position',[2, -20.5, 0.5, 25.5 ],'FaceColor','b');
+rectangle('Position',[50, -20.5, 0.5, 25.5 ],'FaceColor','b');
+rectangle('Position',[2, -20.5, 48, 0.5 ],'FaceColor','b');
+hold on
+%muelle
+rectangle('Position',[-30, -0.5, 30, 0.5 ],'FaceColor','k');
+rectangle('Position',[0, -20, 0.5, 20 ],'FaceColor','k');
 axis([xmin xmax ymin ymax])
